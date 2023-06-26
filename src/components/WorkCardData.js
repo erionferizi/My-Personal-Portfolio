0e2d6470ce976b2e02a4c0dcad8b69a54fc08b02
erionferizi <erion.ferizi@outlook.com>
2023-06-26 10:09:16 +0200

    My Personal Portfolio

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,26 +1,26 @@
-import projectimg1 from "../assets/projects.jpg";
-import projectimg2 from "../assets/projects.jpg";
-import projectimg3 from "../assets/projects.jpg";
+import piggamepic from "../assets/piggamepic.PNG";
+import guessNumber from "../assets/guessmynumbergame.PNG";
+import apiQuote from "../assets/ApiQuote.PNG";
 
 const ProjectCardData = [
   {
-    imgsrc: projectimg1,
-    title: "Pig-game with JavaScript",
-    text: "This is pig game u can play",
+    imgsrc: piggamepic,
+    title: "Piggame with JavaScript",
+    text: "This is the Pig Game, you click roll the dice and the first one to 20 wins",
     view: "https://github.com/erionferizi/JavaScript",
     source: "https://github.com/erionferizi/JavaScript",
   },
   {
-    imgsrc: projectimg2,
-    title: "To Do List App",
-    text: "This is my todolist app with ios",
+    imgsrc: guessNumber,
+    title: "Guess My Number Game",
+    text: "You type a number between 1-20 and click check to find if u guessed correctly to win",
     view: "https://github.com/erionferizi/JavaScript",
     source: "https://github.com/erionferizi/JavaScript",
   },
   {
-    imgsrc: projectimg3,
-    title: "GetQuotes",
-    text: "This is my project with spring boot and javascript getting quotes",
+    imgsrc: apiQuote,
+    title: "Random Quote",
+    text: "Project with SpringBoot and JS, where u can register and create and post quotes",
     view: "https://github.com/erionferizi/JavaScript",
     source: "https://github.com/erionferizi/JavaScript",
   },
